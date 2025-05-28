37ff043ece48bc7a7ac0cdececd8f5b1370f0e92
assbee00 <niuniu9785@hotmail.com>
2025-05-29 01:45:09 +0900

    鸵鸟

--- js/script.js
+++ js/script.js
@@ -1,137 +1,49 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+window.addEventListener('DOMContentLoaded', () => {
+  const imgSwitcher = document.getElementById('js_imgSwitcher');
+  if (imgSwitcher) {
+    const mainImg = document.querySelector('.img-switcher-main img');
+    const thumbBtns = document.querySelectorAll('.img-switcher-btn');
+    
+    // 初期設定（最初のサムネイルをメイン画像にセット）
+    mainImg.src = thumbBtns[0].querySelector('img').src;
+    mainImg.alt = thumbBtns[0].querySelector('img').alt;
+    thumbBtns[0].classList.add('is_active');
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
+    // サムネイルを選択した時の処理
+    thumbBtns.forEach(thumbBtn => {
+      thumbBtn.addEventListener('click', switchImage);
+      thumbBtn.addEventListener('focus', switchImage);
     });
-  });
 
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
+    /**
+     * メイン画像を選択したサムネイル画像に切り替える関数
+     * - メイン画像をフェードアウト
+     * - 0.1秒待機してsrcとaltを変更
+     * - メイン画像をフェードイン
+     * - is_activeクラスを更新
+     */
+    async function switchImage() {
+      const img = this.querySelector('img');
 
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
+      // メイン画像と同じサムネイルが選択された場合は何もしない
+      if (mainImg.getAttribute('src') === img.getAttribute('src')) return;
 
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
+      // メイン画像をフェードアウト
+      mainImg.style.opacity = 0;
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
+      // 0.1秒待機
+      await new Promise(resolve => setTimeout(resolve, 100));
 
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
+      // メイン画像のsrcとaltを更新
+      mainImg.src = img.src;
+      mainImg.alt = img.alt;
 
-      var box = $(html);
+      // メイン画像をフェードイン
+      mainImg.style.opacity = 1;
 
-      $('body').append(box);
+      // サムネイルのis_activeクラスを更新
+      thumbBtns.forEach(thumbBtn => thumbBtn.classList.remove('is_active'));
+      this.classList.add('is_active');
     }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-    });
-
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
   }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
-})(jQuery);
+});
